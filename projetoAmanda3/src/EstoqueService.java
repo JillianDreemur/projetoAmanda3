a499e12ead85f47a3a35f874cde948a7c370fd3a
Mariana de Freitas Albuquerque <104366586+JillianDreemur@users.noreply.github.com>
2024-11-29 15:13:43 -0300

    organizando as ideias

---
+++ projetoAmanda3/src/EstoqueService.java
@@ -0,0 +1,10 @@
+package src;
+
+public class EstoqueService {
+}
+
+//Métodos:
+//adicionarProduto(produto: Produto): Adiciona um novo produto ao estoque.
+//atualizarProduto(id: int, produto: Produto): Atualiza as informações de um produto existente.
+//removerProduto(id: int): Remove um produto do estoque.
+//listarProdutos(): Retorna uma lista de todos os produtos no estoque.
---
+++ projetoAmanda3/src/Produto.java
@@ -0,0 +1,13 @@
+package src;
+
+public class Produto {
+}
+
+
+//Atributos: ID(int), nome do produto(string), categoria(int)(vao ser 3: alimentos, eletronicos e limpeza), quantidade(int), preço(float)
+
+//Métodos:
+//atualizarQuantidade(qtd: int): Atualiza a quantidade
+//atualizarPreco(preco: float): Atualiza o preço
+//atualizarNome(nome: string): atualiza o nome
+//atualizarCategoria(cat: int): atualiza a categoria para uma outra categoria existente
