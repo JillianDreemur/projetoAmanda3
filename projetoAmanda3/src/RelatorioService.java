dfd1ab710bd675795b61f249f5b357a91fe8b61f
Mariana de Freitas Albuquerque <104366586+JillianDreemur@users.noreply.github.com>
2024-12-05 11:18:55 -0300

    Delete projetoAmanda3/src/RelatorioService.java

--- projetoAmanda3/src/RelatorioService.java
+++
@@ -1,8 +0,0 @@
-package src;
-
-public class RelatorioService {
-}
-
-//Métodos:
-//gerarRelatorioMovimentacao(): Gera um relatório de movimentações no estoque.
-//gerarRelatorioPorCategoria(categoria: str): Gera relatório de produtos por categoria.
